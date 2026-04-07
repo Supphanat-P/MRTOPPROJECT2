18bbdd3418466a123aeea56931f99e80766062fd
Nawarut <nawarut.inm@spumail.net>
2026-04-08 04:56:27 +0700

    Jo

---
+++ frontend/src/components/HistoryAdmin.jsx
@@ -0,0 +1,11 @@
+import "../App.css";  
+
+function HistoryAdmin() {
+  return (
+    <div className="HistoryAdmin">
+      <h1>History Packets</h1>
+    </div>
+  );
+}
+
+export default HistoryAdmin;
